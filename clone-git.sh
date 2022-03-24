2926898253cbf57ccda617a8b6a3c7ca93dde12d
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-03-24 12:19:21 +0700

    fix: remove typo

--- clone-git.sh
+++ clone-git.sh
@@ -7,7 +7,7 @@ Blue='\033[0;34m'         # Blue
 input="./urls.txt"
 while IFS= read -r line
 do
-  echo -e "${Blue}Cloning from :${Color_Off} $line !!!\n"
+  echo -e "${Blue}Cloning from :${Color_Off} $line !!!"
   git clone $line
   echo -e "${Green}Success Cloning : ${Color_Off} ${line}\n"
 done < "$input"
